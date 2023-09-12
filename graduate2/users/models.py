115e062a5d48a84e8c16ab6c34d21e2988ae27b8
Adil S <forever@nzironeed.ru>
2023-09-12 21:47:02 +0500

    add user model

--- graduate2/users/models.py
+++ graduate2/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+
+    email = models.CharField(unique=True, max_length=40, verbose_name='user_email')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
