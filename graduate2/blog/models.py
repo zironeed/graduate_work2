647a0801a8baf917793f8bddb1cf4e0264c8c599
Adil S <forever@nzironeed.ru>
2023-09-12 21:31:30 +0500

    add post models

--- graduate2/blog/models.py
+++ graduate2/blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class DefaultPost(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Post_title')
+    description = models.TextField(verbose_name='Post_description')
+    image = models.ImageField(upload_to='media/post/', **NULLABLE, verbose_name='Post_image')
+
+    class Meta:
+        verbose_name = 'Default'
+        verbose_name_plural = 'Defaults'
+
+
+class PremiumPost(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Post_title')
+    description = models.TextField(verbose_name='Post_description')
+    image = models.ImageField(upload_to='media/post/', **NULLABLE, verbose_name='Post_image')
+
+    class Meta:
+        verbose_name = 'Premium'
+        verbose_name_plural = 'Premiums'
