96a2559bb2ce3596e7b8df736b5fb77177bed488
Adil S <forever@nzironeed.ru>
2023-09-12 21:47:56 +0500

    add blog urls.py and update config.urls.py

---
+++ graduate2/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from graduate2.blog.apps import BlogConfig
+
+
+app_name = BlogConfig.name
+
+
+urlpatterns = [
+
+]
--- graduate2/blog/views.py
+++ graduate2/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
-# Create your views here.
+# from graduate2.blog.forms import ProductForm, VersionForm
+from graduate2.blog.models import PremiumPost, DefaultPost
+
+
+class PostListView(ListView):
+    pass
