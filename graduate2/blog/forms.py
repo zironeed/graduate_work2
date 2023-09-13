72de357389205221a09f45b03b63a06b4d841267
Adil S <forever@nzironeed.ru>
2023-09-13 22:37:20 +0500

    add forms and few views

---
+++ graduate2/blog/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from graduate2.blog.models import Post
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = '__all__'
--- graduate2/blog/views.py
+++ graduate2/blog/views.py
@@ -3,9 +3,29 @@ from django.urls import reverse_lazy, reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
 
-# from graduate2.blog.forms import ProductForm, VersionForm
-from graduate2.blog.models import PremiumPost, DefaultPost
+from graduate2.blog.forms import PostForm
+from graduate2.blog.models import Post
 
 
-class PostListView(ListView):
-    pass
+class DefaultPostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'default_list.html'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner__isnull=True)
+
+
+class PremiumPostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'premium_list.html'
+
+    def get_queryset(self):
+        return super().get_queryset().filter(owner__isnull=False)
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+    success_url = reverse_lazy('blog:premium_list',)
