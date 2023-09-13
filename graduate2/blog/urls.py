614189d87f810920a7a5e0e20a21378ed714d0ca
Adil S <forever@nzironeed.ru>
2023-09-13 22:37:33 +0500

    update urls and settings

--- graduate2/blog/urls.py
+++ graduate2/blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from graduate2.blog.apps import BlogConfig
 
+from graduate2.blog.apps import BlogConfig
+from graduate2.blog.views import DefaultPostListView, PremiumPostListView, PostCreateView
 
 app_name = BlogConfig.name
 
 
 urlpatterns = [
-
+    path('blog/posts/free', DefaultPostListView.as_view(), name='default_list'),
+    path('blog/posts/premium', PremiumPostListView.as_view(), name='premium_list'),
+    path('blog/posts/create', PostCreateView.as_view(), name='post_create'),
 ]
--- graduate2/config/settings.py
+++ graduate2/config/settings.py
@@ -137,4 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'graduate2.users.User'
