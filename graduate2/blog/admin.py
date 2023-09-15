a63f2e289d20339eebc42511c9b65780c04fddf6
Adil S <forever@nzironeed.ru>
2023-09-15 21:14:18 +0500

    add admin for blog app

--- graduate2/blog/admin.py
+++ graduate2/blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'image', 'owner',)
+    list_filter = ('owner',)
+    search_fields = ('title', 'owner',)
