1b1fbaa5efb3bbb03ae7942b1af4e640403bdfe4
Adil S <forever@nzironeed.ru>
2023-09-16 16:16:52 +0500

    add login and logout views and templates

--- graduate2/users/views.py
+++ graduate2/users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView as BaseLogin
+from django.contrib.auth.views import LogoutView as BaseLogout
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, \
+    PasswordResetCompleteView, PasswordResetDoneView
 
-# Create your views here.
+from django.views.generic import CreateView, TemplateView
+
+from django.urls import reverse_lazy
+
+from .models import User
+
+
+class TitleMixin(object):
+    title = None
+
+
+class LoginView(TitleMixin, BaseLogin):
+    template_name = 'users/login.html'
+    title = 'Log in'
+
+
+class LogoutView(TitleMixin, BaseLogout):
+    template_name = 'users/logout.html'
+    title = 'Log out'
