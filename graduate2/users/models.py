992fd23e553c7a3c769f88434b87bcf9b651eb15
Adil S <forever@nzironeed.ru>
2023-09-16 16:10:06 +0500

    add class UserPayment and payment field in class User

--- graduate2/users/models.py
+++ graduate2/users/models.py
@@ -1,11 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from blog.models import NULLABLE
+
 
 class User(AbstractUser):
     username = None
 
     email = models.CharField(unique=True, max_length=40, verbose_name='user_email')
+    payment = models.OneToOneField('UserPayment', on_delete=models.CASCADE,
+                                   verbose_name='User_payment', related_name='user_payment', **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -13,3 +17,13 @@ class User(AbstractUser):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+
+class UserPayment(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE, verbose_name='Payment_user')
+    payment_id = models.CharField(max_length=100, verbose_name='Payment_id')
+    paid = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Payment'
+        verbose_name_plural = 'Payments'
