2b02d1b82136526bd9d1610d0593c7e39a50e457
Adil S <forever@nzironeed.ru>
2023-09-21 12:05:58 +0500

    update users/models.py

--- graduate2/users/models.py
+++ graduate2/users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from blog.models import NULLABLE
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class User(AbstractUser):
@@ -10,6 +12,7 @@ class User(AbstractUser):
     phone_number = models.CharField(unique=True, max_length=10, verbose_name='User_phone (without +7)', **NULLABLE)
     payment = models.OneToOneField('UserPayment', on_delete=models.CASCADE,
                                    verbose_name='User_payment', related_name='user_payment', **NULLABLE)
+    is_community = models.BooleanField(default=False, verbose_name='User_is_community')
 
     USERNAME_FIELD = "phone_number"
     REQUIRED_FIELDS = []
@@ -22,8 +25,14 @@ class User(AbstractUser):
 class UserPayment(models.Model):
     user = models.OneToOneField('User', on_delete=models.CASCADE, verbose_name='Payment_user')
     payment_id = models.CharField(max_length=100, verbose_name='Payment_id')
-    paid = models.BooleanField(default=False)
+    is_paid = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = 'Payment'
         verbose_name_plural = 'Payments'
+
+
+@receiver(post_save, sender=User)
+def create_user_payment(sender, instance, created, **kwargs):
+    if created:
+        UserPayment.objects.create(user=instance)
