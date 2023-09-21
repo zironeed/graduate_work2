e3ab21b65a0e85af84712e122a039b34778502df
Adil S <forever@nzironeed.ru>
2023-09-21 19:16:31 +0500

    add admin.py for users

--- graduate2/users/admin.py
+++ graduate2/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('phone_number', 'password')
+    list_filter = ('phone_number',)
+    search_fields = ('phone_number',)
