9a30e95bfd2fb8aebe7ac8c28da5c3d20420b2e5
Adil S <forever@nzironeed.ru>
2023-09-21 19:08:16 +0500

    add tests.py

--- graduate2/users/tests.py
+++ graduate2/users/tests.py
@@ -1,3 +1,37 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestBlog(TestCase):
+
+    def test_view_url_exists_login(self):
+        resp = self.client.get('/users/login/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_exists_registration(self):
+        resp = self.client.get('/users/user/registration/')
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_accessible_by_name_login(self):
+
+        resp = self.client.get(reverse('users:login'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_accessible_by_name_registration(self):
+
+        resp = self.client.get(reverse('users:user_register'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_login_uses_correct_template(self):
+
+        resp = self.client.get(reverse('users:login'))
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, 'users/login.html')
+
+    def test_registration_uses_correct_template(self):
+
+        resp = self.client.get(reverse('users:user_register'))
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertTemplateUsed(resp, 'users/register.html')
