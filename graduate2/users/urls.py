657d7eb5476d4dc4a9390f95c307ea36ac11f343
Adil S <forever@nzironeed.ru>
2023-09-21 12:05:01 +0500

    payment urls

--- graduate2/users/urls.py
+++ graduate2/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
 from .apps import UsersConfig
-from .views import RegisterView, LoginView, LogoutView, PaymentView
+from .views import RegisterView, LoginView, LogoutView, product_page, \
+    product_cancelled, product_success, webhook
 
 app_name = UsersConfig.name
 
@@ -11,5 +12,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
 
     path('user/registration/', RegisterView.as_view(), name='user_register'),
-    path('user/payment', PaymentView.as_view(), name='user_payment')
+
+    path('user/payment/', product_page, name='payment'),
+    path('payment_successful', product_success, name='payment_successful'),
+    path('payment_cancelled', product_cancelled, name='payment_cancelled'),
+    path('stripe_webhook', webhook, name='webhook'),
 ]
